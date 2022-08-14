440afb95fa67661bec30ac92e043dca26f0d3f39
dnk363 <dnk363@live.ru>
2022-08-14 12:33:03 +0300

    +

--- IsInclude/Program.cs
+++ IsInclude/Program.cs
@@ -7,6 +7,7 @@ namespace IsInclude
         static void Main()
         {
             int[] array = new int[] { 1, 2, 3, 3, 3, 5, 7, 9, 11, 15, 21, 22, 29, 38, 56, 101 };
+
             int[] subArray1 = new int[] { 3, 3, 3, 3, 5, 7 };
             int[] subArray2 = new int[] { 3, 5, 7 };
             int[] subArray3 = new int[] { 3, 3, 5, 7 };
